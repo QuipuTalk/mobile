3fcb2f24eb407d7eaa35553b85baeb4c9d3d525f
Alyne <alyn.remo@gmail.com>
2024-09-02 22:37:41 -0500

    camera settings

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:quiputalk/screens/camera_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: const Text('Home Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Bienvenido a Quipu Talk',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Acción para grabar video, puedes cambiar esto según la funcionalidad que quieras
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context)=> const CameraScreen())
+                );
+                 
               },
-              child: Text('Grabar Video'),
+              child: const Text('Grabar Video'),
             ),
             ElevatedButton(
               onPressed: () {
                 // Acción para ver traducción, puedes cambiar esto según la funcionalidad que quieras
               },
-              child: Text('Ver Traducción'),
+              child: const Text('Ver Traducción'),
             ),
           ],
         ),
---
+++ lib/screens/video_screen.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoScreen extends StatefulWidget {
+  final String videoPath;
+
+  const VideoScreen({super.key, required this.videoPath});
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(File(widget.videoPath));
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(false); 
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); 
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Video"),
+      ),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Muestra el video una vez que está listo
+            return Center(
+              child: AspectRatio(
+             aspectRatio: MediaQuery.of(context).size.aspectRatio, // Ajusta a la pantalla
+                child: VideoPlayer(_controller),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Reproduce o pausa el video
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
